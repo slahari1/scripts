9975cc56609b01e825bdf57c96460dedab0fb841
lahari1 <lahari1@live.unc.edu>
2017-07-14 00:15:38 -0400

    first script entry added

---
+++ dirinfo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ""
+echo "Current directory:" `pwd` 
+echo ""
+echo "Files in directory:" `ls -lah`
+echo ""
+echo "Today's date is:" `date`
+echo ""
+echo "System name is:" `hostname`
+echo ""
+echo "IP address is:" `hostname -i`
